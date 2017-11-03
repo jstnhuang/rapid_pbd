0e25b14e326adb09429bdd15b3d20cc2ed2556b6
Justin Huang <jstn@cs.washington.edu>
2017-11-02 18:42:39 -0700

    Pass arg to PublishCollisionObject as a const ref.

--- src/motion_planning.cpp
+++ src/motion_planning.cpp
@@ -272,7 +272,7 @@ std::string ErrorCodeToString(const MoveItErrorCodes& code) {
   return ss.str();
 }
 
-void MotionPlanning::PublishCollisionObject(moveit_msgs::CollisionObject& obj) {
+void MotionPlanning::PublishCollisionObject(const moveit_msgs::CollisionObject& obj) {
   moveit_msgs::PlanningScene scene;
   scene.world.collision_objects.push_back(obj);
   scene.is_diff = true;
